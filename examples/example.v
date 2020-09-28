ea689235f413086c8d2966507d83e8463f36cccb
pancake <pancake@nopcode.org>
2020-09-28 12:18:06 +0200

    Add spawn example

---
+++ examples/example.v
@@ -0,0 +1,8 @@
+import r2pipe
+
+fn main() {
+	mut r2 := r2pipe.spawn('/bin/ls', '') or { panic (err) }
+	r := r2.cmd('?e hell')
+	println(r)
+	r2.free()
+}
--- r2pipe_test.v
+++ r2pipe_test.v
@@ -2,7 +2,16 @@ module r2pipe
 
 fn test_main() {
 	mut r2 := new()
-	println(r2.cmd('i')) // ?e hello world'))
+	println(r2.cmd('i'))
 	r2.free()
+}
+
+fn test_spawn() {
+	// XXX this is hanging under v test
+/*
+	mut r2 := spawn('/bin/ls', '') or { panic(err) }
+	println(r2.cmd('i'))
+	r2.free()
+*/
 }
 
