ecbd9a4e1909cf4cbfb6d9a84d0539f6895d371c
pancake <pancake@nopcode.org>
2022-12-18 17:55:04 +0100

    WIP json fixes for the js backend

--- examples/qjstest.v
+++ examples/qjstest.v
@@ -4,4 +4,8 @@ import r2pipe
 fn main() {
 	res := r2pipe.cmd("x")
 	println("hello: " + res)
+	/*
+	j := r2pipe.cmdj("ij")
+	println("file: " + file)
+	*/
 }
--- r2pipe.js.v
+++ r2pipe.js.v
@@ -1,17 +1,13 @@
 module r2pipe
 
-import json
-
 pub fn cmd(a string) string {
 	mut res := ""
 	#res = r2cmd(a);
 	return res
 }
 
-/*
-pub fn cmdj(a string) {
+pub fn cmdj(a string) JS.Any {
 	mut res := ""
 	#res = JSON.parse(r2cmd(a));
 	return res
 }
-*/
