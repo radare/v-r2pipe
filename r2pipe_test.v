a36a5bc2c6482fc7d333c6c678e0a65ebd4a6ed6
pancake <pancake@nopcode.org>
2024-07-31 11:56:41 +0200

    Improve tests

--- r2pipe_test.v
+++ r2pipe_test.v
@@ -1,14 +1,24 @@
 module r2pipe
 
 fn test_main() {
+	// running an r2pipe local script from outside r2pipe
 	mut r2 := new()
-	println(r2.cmd('i'))
+	res := r2.cmd('i')
+	if res != "" {
+		panic("Expected an 'Cannot find R2PIPE_IN|OUT' error")
+	}
 	r2.free()
 }
 
 fn test_spawn() {
 	// XXX this is hanging under v test
 	mut r2 := r2spawn('/bin/ls', '') or { panic(err) }
-	println(r2.cmd('i'))
+	res := r2.cmd('i')
+	idx := res.index('file') or { panic(err) }
+	if idx > 0 {
+		println(r2.cmd('i'))
+	} else {
+		panic("Expected file in i output")
+	}
 	r2.free()
 }
